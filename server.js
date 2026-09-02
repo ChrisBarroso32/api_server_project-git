a06291fdea7a570b99b84788a802d53c851613ce
Christian <chrisbarroso32@gmail.com>
2026-09-01 18:37:53 -0600

    Correxión de en las líneas 9 y 16 en el script de server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 const port = 3000;
 
-app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.send('¡Hola Mundo!');
 });
 
 app.listen(port, () => {
-    console.log(`Aplicación de ejemplo en http://localhost${port}`)
+    console.log(`Aplicación de ejemplo en http://localhost:${port}`)
 })
